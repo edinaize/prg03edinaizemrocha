6c4e1f2f5836f7fa14370fa621a7589af29aa746
Edinaize Machado Rocha <edinaize.r@gmail.com>
2026-09-02 16:35:49 -0300

    Estrutura inicial do projeto Maven

---
+++ prg03edinaizemrocha/src/main/java/br/com/ifba/login/view/Prg03edinaizemrocha.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.ifba.login.view;
+
+/**
+ *
+ * @author edina
+ */
+public class Prg03edinaizemrocha {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
