04d8b53fe09c68721f3ead74a46bc64bc8b94491
Edinaize Machado Rocha <edinaize.r@gmail.com>
2026-09-23 13:57:37 -0300

    feat: adiciona enum de status da obra
    Cria o enum StatusObra com os estados da obra e utiliza o tipo enumerado no atributo status da classe Obra.

---
+++ prg03edinaizemrocha/src/main/java/br/com/ifba/obra/entity/StatusObra.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package br.com.ifba.obra.entity;
+
+/**
+ *
+ * @author edina
+ */
+public enum StatusObra {
+    RASCUNHO, PUBLICADA, ARQUIVADA
+}
